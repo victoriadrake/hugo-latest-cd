58dd1eb4da4c59235ab82513320387380c57d4bb
Victoria Drake <hello@victoria.dev>
2020-09-06 15:55:06 -0400

    🌱 Support destination and build option variables

--- action.sh
+++ action.sh
@@ -16,16 +16,16 @@ cd ${GITHUB_WORKSPACE}
 hugo version || exit 1
 
 echo '🧹 Clean site'
-if [ -d "docs" ]; then
-    rm -rf docs/*
+if [ -d "${DEST:-"docs"}" ]; then
+    rm -rf ${DEST:-"docs"}/*
 fi
 
 echo '🍳 Build site'
-hugo -d docs
+hugo ${HUGO_ARGS:-""} -d ${DEST:-"docs"}
 
 echo '🚀 Deploy build'
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -am "🚀 Deploy with ${GITHUB_WORKFLOW}"
-git push --all -f https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+git push --all -f https://${TOKEN}@github.com/${GITHUB_REPOSITORY}.git
